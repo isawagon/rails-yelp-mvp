01857dce54f65fc169a8cbc0ab9b9dfd93af0dac
isabelle levy <isabel.levy@icloud.com>
2022-04-19 21:39:24 +0200

    controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+puts "Creating restaurants..."
+kom = { name: 'Kom a la maison', address: '97223 Le Diamant',
+      phone_number: '+596010101', category: 'french'}
+
+[kom].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
