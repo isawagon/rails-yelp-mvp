7660a5738ea59a984eb7789a4a949871d117e79f
isabelle levy <isabel.levy@icloud.com>
2022-04-20 12:07:49 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # db/seeds.rb
 
-# puts "Cleaning database..."
-# Restaurant.destroy_all
+puts "Cleaning database...#{Restaurant.count} restaurants ... #{Review.count} reviews"
+Review.delete_all
+Restaurant.delete_all
 
 puts "Creating restaurants..."
 kom = { name: 'Kom a la maison', address: '97223 Le Diamant',
@@ -18,3 +19,23 @@ kom = { name: 'Kom a la maison', address: '97223 Le Diamant',
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+  [0, 1, 2].sample.times do
+    review = Review.new(
+      restaurant_id: restaurant.id,
+      rating: [0, 1, 2, 3, 4, 5].sample,
+      content: Faker::Restaurant.review
+    )
+    review.save
+  end
+end
+puts "Finished! #{Restaurant.count} restaurants and #{Review.count} reviews created"
